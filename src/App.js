a4021b92d94d7d6252481c33157e812d053b4cc8
Learning171 <learningskills171@gmail.com>
2024-02-28 21:54:44 +0530

    pages commited

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import SignIn from "./components/LoginForm";
+// import Navbar from "./components/Navbar";
+import SignUp from "./components/RegistrationForm";
+import HomePage from "./components/HomePage";
+// import ChangePassword from "./components/ChangePasswordForm";
+// import ForgetPassword from "./components/ForgetPasswordForm";
+// import { Routes, Route} from "react-router-dom";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap.bundle.min";
+
+function App() {
+  return (
+    <>
+      {/* <Navbar /> */}
+      <HomePage />
+      <SignIn />
+      <SignUp />
+      {/* <ChangePassword />
+      <ForgetPassword /> */}
+      {/* <Routes>
+        <Route path="/register" element={<SignUp />}/>
+        <Route path="/login" element={<SignIn />}/>
+      </Routes> */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Menu,
+  MenuItem,
+  Button,
+} from "@mui/material";
+import { ShoppingCart, AccountCircle } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // Track user's authentication status
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+ 
+
+  const handleLogout = () => {
+    // Implement logic for handling logout
+    setIsLoggedIn(false);
+    setAnchorEl(null);
+  };
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Project Name
+        </Typography>
+        <Button color="inherit">Home</Button>
+        <Button color="inherit">Become Seller</Button>
+        <IconButton color="inherit">
+          <ShoppingCart />
+        </IconButton>
+        {isLoggedIn ? (
+          // Display user's avatar if logged in
+          <IconButton color="inherit">
+            <AccountCircle onClick={handleClick} />
+            <Menu
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+            >
+              <MenuItem onClick={handleClose}>My Profile</MenuItem>
+              <MenuItem onClick={handleClose}>Orders</MenuItem>
+              <MenuItem onClick={handleClose}>Change Password</MenuItem>
+              <MenuItem onClick={handleLogout}>Logout</MenuItem>
+            </Menu>
+          </IconButton>
+        ) : (
+          // Display "Login" and "Register" menu items if not logged in
+          <>
+             
+              <div>
+                <Link to="/login">
+                <Button color="inherit" >
+                <Typography sx={{ color: "white", padding: 0 }}>Login</Typography>
+                </Button>
+                </Link>
+
+              </div><div>
+                <Link to="/register">
+                  <Typography sx={{ color: "white", padding: 0 }}>Register</Typography>
+                </Link>
+              </div></>
+            
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
